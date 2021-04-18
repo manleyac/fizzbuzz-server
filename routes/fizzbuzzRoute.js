e48d9907faf720075ec6386b6f5899ee05860861
Andrew <8879789+manleyac@users.noreply.github.com>
2021-04-18 18:09:29 -0400

    creating function fizzbuzz to handle logic for route /fizzbuzz/

--- routes/fizzbuzzRoute.js
+++ routes/fizzbuzzRoute.js
@@ -2,10 +2,27 @@ const express = require("express");
 
 const router = express.Router();
 
+function fizzbuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  } else if (num % 3 == 0) {
+    return "Fizz";
+  } else if (num % 5 == 0) {
+    return "Buzz";
+  } else {
+    return num;
+  }
+}
+
+// @route   GET fizzbuzz/:num
+// @param   An integer
+// @desc    Takes an integer, passes it to fizzbuzz function and returns result
+// @access  Public
 router.get("/:num", (req, res) => {
   const input = req.params.num;
   const number = Number(input);
-  res.json({ result: number });
+  const result = fizzbuzz(number);
+  res.json({ result: result });
 });
 
 module.exports = router;
