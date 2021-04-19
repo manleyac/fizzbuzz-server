06bac1c0382a9a45ddedd95c1c7288f5993fad2c
Andrew <8879789+manleyac@users.noreply.github.com>
2021-04-18 20:09:23 -0400

    simplifying primeFactor function

--- functions/primefactors.js
+++ functions/primefactors.js
@@ -1,31 +1,24 @@
-
 //accepts a positive integer and returns array of prime factors for that number
 function primeFactors(num) {
-   const factors = [];
-   let prime = 2;
-   let number = num;
-  
-   while(number % prime === 0) {
-      number = number / prime;
+  const factors = [];
+  let prime = 2;
+  let number = num;
+
+  for (let i = 2; i <= number; i++) {
+    for (let j = 2; j < prime; j++) {
+      if (prime % j === 0) {
+        prime++;
+      }
+    }
+
+    while (number % prime === 0) {
       factors.push(prime);
-   }
+      number = number / prime;
+    }
+    prime++;
+  }
 
-   for(let i = 2; i <= number; i++ ) {
-      for(let j = 2; j < prime; j++) {
-         if(prime % j === 0){
-            prime++;
-         }
-      }
-
-      if(number % prime === 0) {
-         factors.push(prime);
-         number = number / prime;
-      } else {
-         prime++;
-      }
-   }
-
-   return factors;
+  return factors;
 }
 
 module.exports = primeFactors;
