7a781e71db760708583f5b1f135cb7f2a771c03d
Andrew <8879789+manleyac@users.noreply.github.com>
2021-05-12 19:07:51 -0400

    refactoring express app into seperate file

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const express = require("express");
 
-const fizzbuzzRoute = require("./routes/fizzbuzzRoute.js");
-const primeFactorsRoute = require("./routes/primeFactorsRoute.js");
+const app = require("./app.js");
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.use("/fizzbuzz", fizzbuzzRoute);
-app.use("/primefactors", primeFactorsRoute);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ routes/fizzbuzzRoute.test.js
@@ -0,0 +1,5 @@
+describe("Test Get /fizzbuzz", () => {
+   test("",() => {
+      
+   });
+})
